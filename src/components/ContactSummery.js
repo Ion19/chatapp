107ac1888fc71da8ea53f52f0e5fdb9fe18dc090
Ion19 <engabdelrhman2016@gmail.com>
2019-06-19 20:44:27 +0200

    update the contact component

--- src/components/ContactSummery.js
+++ src/components/ContactSummery.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle } from 'reactstrap';
+  CardTitle, CardSubtitle, InputGroupAddon ,Input ,InputGroup } from 'reactstrap';
   import {connect} from 'react-redux'; 
   import { activateUserAction } from './../store/actions/actions';
 
@@ -17,20 +17,27 @@ class ContactSummery extends React.Component {
     <div>
       
         <Card>
-          
-        {this.props.usersList.map((user)=>( <Card>
-         
-            <CardBody className={`${user.active ? "active" : ""}`}  onClick={() => this.props.activateUser(user)}>
-            <CardImg top width="100%" className="rounded-circle" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="contactImage" />  
-              <CardTitle>{user.firstName} {user.lastName}</CardTitle>
-              <CardSubtitle>
-                <small className="text-muted">{(user.messages[0])?(user.messages[0].date):(<h5>--</h5>)}</small>
-              </CardSubtitle>
-              <CardText>{(user.messages[0])?(user.messages[0].msg):(<h5>Click to start chat now</h5>)}</CardText>
-              
-            </CardBody>
+        <div>
+          <InputGroup>
+            <InputGroupAddon addonType="prepend">find</InputGroupAddon>
+            <Input placeholder="Seach Contact" />
+          </InputGroup>
+         </div>  
+        {this.props.usersList.map((user)=>( 
+        ((user.messages[0]) ? (<Card>
+        
+          <CardBody className={`${user.active ? "active" : ""}`}  onClick={() => this.props.activateUser(user)}>
+          <CardImg top width="100%" className="rounded-circle" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="contactImage" />  
+            <CardTitle>{user.firstName} {user.lastName}</CardTitle>
+            <CardSubtitle>
+              <small className="text-muted">{(user.messages[0])?(user.messages[0].date):(<h5>--</h5>)}</small>
+            </CardSubtitle>
+            <CardText>{(user.messages[0])?(user.messages[0].msg):(<h5>Click to start chat now</h5>)}</CardText>
             
-          </Card>)) }
+          </CardBody>
+          
+        </Card>): (null))
+        )) }
           
           
 
